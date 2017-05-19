b1da9dde1d8a353074a977b002cfd13f0044b5d5
kayfelicities <390528655@qq.com>
2017-05-19 22:09:51 +0800

    dev log

---
+++ dev_log.py
@@ -0,0 +1,21 @@
+'''develop log'''
+LOG = '''
+  <header>
+    <h3><i></i> 开发日志 <small></small></h3>
+  </header>
+  <hr>
+
+  <h4>2017.05.19:</h4>
+  <ol>
+    <li>基本功能实现</li>
+  </ol>
+
+  <hr>
+  
+  <h4>todo list:</h4>
+  <ol>
+    <li>删除无效列对应的数据，保存相关日志</li>
+    <li>运行日志优化</li>
+    <li>errlog优化</li>
+  </ol>
+'''
